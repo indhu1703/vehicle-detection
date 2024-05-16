b92a877da0f22c202eaddbc1c9aeaaa3f39948cd
Indhu Prakash S <141543446+indhu1703@users.noreply.github.com>
2024-05-16 09:59:35 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Car_deduction
+This project will deduct the car which passing in the video. This project need xml file and python file.
---
+++ car.py
@@ -0,0 +1,28 @@
+
+import cv2
+cars_cascade = cv2.CascadeClassifier('cars.xml')
+
+def detect_cars(frame):
+    cars = cars_cascade.detectMultiScale(frame, 1.15, 4)
+    for (x, y, w, h) in cars:
+        cv2.rectangle(frame, (x, y), (x+w,y+h), color=(0, 255, 0), thickness=2)
+    return frame
+  
+def Simulator():
+    CarVideo = cv2.VideoCapture('car1.mp4')
+    while CarVideo.isOpened():
+        ret, frame = CarVideo.read()
+        controlkey = cv2.waitKey(1)
+        if ret:        
+            cars_frame = detect_cars(frame)
+            cv2.imshow('frame', cars_frame)
+        else:
+            break
+        if controlkey == ord('q'):
+            break
+
+    CarVideo.release()
+    cv2.destroyAllWindows()
+    
+if __name__ == '__main__':
+    Simulator()
